e442abac3301d555cb1c2c59c32fb10899be77c8
jorge soberanis gonzález <jorgesoberanis174@gmail.com>
2025-02-20 12:34:55 -0600

    mensaje reactivo

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,19 @@
 <script setup>
   import Sidebar from '@/components/Sidebar.vue';
   import Header from '@/components/Header.vue';
+  import { ref } from 'vue';
+
+  const texto = ref("");
+
 </script>
 
 <template>
-  <Header></Header>
+  <Header :mensaje="texto"></Header>
   <div class="container">
     <Sidebar></Sidebar>
     <div class="contenido">
+      <input type="text" v-model="texto">
+      {{ texto  }}
       <router-view></router-view>
     </div>
   </div>
